1ad463c8f042f37a570f933ea4af761cb22931ac
Agustin Braco <bracoagustin@gmail.com>
2025-05-25 21:30:48 -0300

    Revert "Subida de mejoras: board, resumen, login y visuales"
    This reverts commit 5d1ae1d8cc3a8f813c78ddce6c7461cadfb43665.

--- web_task-manager-main/src/components/index.js
+++
@@ -1,9 +0,0 @@
-export { Bar } from './Bar.jsx'
-export { Tasks } from './Tasks.jsx'
-export { Task } from './Task.jsx'
-export { Modal } from './Modal.jsx'
-export { Add } from './Add.jsx'
-export { Search } from './Search'
-export { SortSelector } from './SortSelector'
-export { Navbar } from './Navbar'
-
